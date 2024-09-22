5d092bb2ca87a943d72902308153bb57bb28e92a
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2024-09-22 17:05:14 +0100

    feat: library

--- README.md
+++ README.md
@@ -4,21 +4,18 @@ A command-line tool to import recipes into [Cooklang](https://cooklang.org/) for
 ## Getting started
 
 1. Make sure you have the following prerequisites:
-    * [Python 3](https://www.python.org/downloads/)
-    * [Poetry](https://pypi.org/project/poetry/)
+    * [Rust](https://www.rust-lang.org/tools/install)
+    * [OpenAI API key](https://platform.openai.com/api-keys) set in your environment variables as `OPENAI_API_KEY`
 2. Clone this repo locally
-3. Change directory into this repo root and run `poetry install`
+3. Change directory into this repo root and run `cargo install --path .`
 
 ## Usage examples
 ### See all available flags
 ```
-    poetry run ./cook-import --help
+cook-import --help
 ```
 ### Scrape a recipe from a webpage and output to screen
 ```
-    poetry run ./cook-import --link https://www.bbcgoodfood.com/recipes/next-level-tikka-masala
+cook-import https://www.bbcgoodfood.com/recipes/next-level-tikka-masala
 ```
-### Scrape a recipe from a webpage and output to file
-```
-    poetry run ./cook-import --link https://www.bbcgoodfood.com/recipes/next-level-tikka-masala --file
-```
+
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
-use log::{debug, error};
-use reqwest::blocking::get;
-use scraper::Html;
+use cooklang_import::import_recipe;
 use std::env;
-
-mod converters;
-mod extractors;
-mod model;
-
-use crate::converters::ConvertToCooklang;
-use crate::extractors::Extractor;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the URL from command-line arguments
     let args: Vec<String> = env::args().collect();
     let url = args.get(1).ok_or("Please provide a URL as an argument")?;
 
-    // Fetch the webpage content
-    let body = get(url)?.text()?;
-
-    // Parse the HTML document
-    let document = Html::parse_document(&body);
-
-    let extractor = extractors::JsonLdExtractor;
-    if extractor.can_parse(&document) {
-        let recipe = extractor.parse(&document)?;
-        debug!("{:#?}", recipe);
-
-        let openai_api_key =
-            env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set in the environment");
-        let converter = converters::OpenAIConverter::new(openai_api_key);
-        let cooklang_recipe = converter.convert(&recipe.ingredients, &recipe.steps)?;
-        println!("{}", cooklang_recipe);
-    } else {
-        error!("Unable to parse the recipe from this webpage.");
-    }
+    // Import the recipe
+    let cooklang_recipe = import_recipe(url)?;
+    println!("{}", cooklang_recipe);
 
     Ok(())
 }
