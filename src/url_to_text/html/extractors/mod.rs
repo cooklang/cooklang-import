0a8dc79d1de033e277f473117b0f64c096d65bb8
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2025-12-05 21:31:22 +0000

    feat: move JsonLdExtractor to url_to_text/html/extractors

--- src/url_to_text/html/extractors/mod.rs
+++ src/url_to_text/html/extractors/mod.rs
@@ -1,1 +1,16 @@
-// Extractors: json_ld.rs, microdata.rs, html_class.rs
+use crate::model::Recipe;
+use scraper::Html;
+
+mod json_ld;
+
+pub use json_ld::JsonLdExtractor;
+
+pub struct ParsingContext {
+    pub url: String,
+    pub document: Html,
+    pub texts: Option<String>,
+}
+
+pub trait Extractor {
+    fn parse(&self, context: &ParsingContext) -> Result<Recipe, Box<dyn std::error::Error>>;
+}
